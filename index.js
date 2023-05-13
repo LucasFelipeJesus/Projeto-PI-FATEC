799936969b8d8e9d296ca62b8705a8262b1e4f8f
Luis Gaspari <luisgaspari@gmail.com>
2023-05-13 09:43:37 -0300

    Adiciona link para Vendas em "index.js"

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import ClienteRouter from "./Routes/ClienteRoute.js"
 import ProdutoRouter from "./Routes/ProdutoRoute.js"
 import ComprasRouter from "./Routes/ComprasRoute.js"
 import DetComprasRouter from "./Routes/DetComprasRoute.js"
+import VendasRouter from "./Routes/VendasRoute.js"
+import DetVendasRouter from "./Routes/DetVendasRoute.js"
 
 const app = express()
 app.use(express.json())
@@ -11,5 +13,7 @@ app.use("/clientes", ClienteRouter)
 app.use("/produtos", ProdutoRouter)
 app.use("/Compras", ComprasRouter)
 app.use("/DetalhesCompras", DetComprasRouter)
+app.use("/Vendas", VendasRouter)
+app.use("/DetalhesVendas", DetVendasRouter)
 
 app.listen(3000, () => console.log("API Rodando na porta 3000"))
