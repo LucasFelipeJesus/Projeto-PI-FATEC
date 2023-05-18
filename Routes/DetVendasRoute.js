b819be7fc953619030988d20f1fa149426e24cbc
Luis Gaspari <luisgaspari@gmail.com>
2023-05-18 18:38:23 -0300

    Atualiza GRUD de Vendas (Model-Controller-Route)

--- Routes/DetVendasRoute.js
+++ Routes/DetVendasRoute.js
@@ -4,5 +4,9 @@ import DetVendasController from "../Controller/DetVendasController.js";
 const DetVendasRouter = express.Router();
 
 DetVendasRouter.get("/", DetVendasController.getDetVenda);
+DetVendasRouter.post("/", DetVendasController.createDetVenda);
+DetVendasRouter.get("/:idPedido", DetVendasController.getDetPedidoVendaById);
+DetVendasRouter.delete("/:idPedido", DetVendasController.destroyDetPedidoVenda);
+DetVendasRouter.put("/:idPedido", DetVendasController.updateDetPedidoVenda);
 
 export default DetVendasRouter;
