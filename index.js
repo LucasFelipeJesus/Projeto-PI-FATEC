1e3a4217132e4ff1c8ae04a68540a4fbf87d6ebc
Caio Assis <caio.ab.assis@gmail.com>
2023-05-20 16:14:28 -0300

    Add db.js, mod index.js, instala sequelize

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express"
+import db from './db.js'
+
 import ClienteRouter from "./Routes/ClienteRoute.js"
 import ProdutoRouter from "./Routes/ProdutoRoute.js"
 import ComprasRouter from "./Routes/ComprasRoute.js"
@@ -9,11 +11,15 @@ import DetVendasRouter from "./Routes/DetVendasRoute.js"
 const app = express()
 app.use(express.json())
 
+db.sync(() => console.log('Banco de dados preparado'))
+
 app.use("/clientes", ClienteRouter)
 app.use("/produtos", ProdutoRouter)
 app.use("/Compras", ComprasRouter)
 app.use("/DetalhesCompras", DetComprasRouter)
 app.use("/Vendas", VendasRouter)
 app.use("/DetalhesVendas", DetVendasRouter)
+
+const port = process.env.PORT || 3000
 
 app.listen(3000, () => console.log("API Rodando na porta 3000"))
