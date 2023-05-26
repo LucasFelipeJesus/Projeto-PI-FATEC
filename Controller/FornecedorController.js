eba9f28dcd977b485dea015d80759440c218b3d3
Alessandra Freitas <114964603+aletfreitas@users.noreply.github.com>
2023-05-26 08:05:20 -0300

    Altera para controller fornecedor para API

--- Controller/FornecedorController.js
+++ Controller/FornecedorController.js
@@ -1,70 +1,62 @@
-import { Fornecedor, findAllFornecedor, create, findByPk, destroy, update } from "c:/Users/User/Desktop/PIC 2/Projeto-PI-FATEC/Controller/FornecedorController.js"
+import Fornecedor from "..Controller/FornecedorController.js"
 
 class FornecedorController {
-    static getFornecedor(req,res){
-        res.json(findAllFornecedor())
+    static async list (req,res){
+        const fornecedores = await Fornecedor.findAll()
+        res.json(fornecedores)
     }
 
-    static createFornecedor(req,res){
-        const {nome, cnpj, telefone, cep, estado, cidade, bairro, endereco, complemento, email} = req.body
-        if(!nome || !cnpj || !telefone || !cep || !estado || !cidade || !bairro || !endereco || !complemento || !email){
+    static async getFornecedorById (req,res){
+        const id = parseInt(req.params.id)
+        const fornecedor = await Fornecedor.findByPk(id)
             res.status(400).json({error: "Informe todos os campos!"})
-            return
+            
+        if (!fornecedor){
+            res.status(404).json({error:"Não encontrado"})
+        }
+        res.json(fornecedor)
         }
 
-        const Fornecedor = new Fornecedor (nome, cnpj, telefone, cep, estado, cidade, bairro, endereco, complemento, email)
-        create(Fornecedor)
-        res.json(Fornecedor)
+        static async destroyFornecedor(req,res){
+            const id = parseInt(req.params.id)
+            const fornecedor = await Fornecedor.findByPk(id)
+            if(!fornecedor){
+                res.status(404).json({error:"Não encontrado"})
+                return
+            }
+            await Fornecedor.destroy({where: {id: fornecedor.id}})
+            res.json({message: "Removido com sucesso!"})
+        }    
+
+        static async createFornecedor(req,res){
+            const {nome, cnpj, telefone, cep, estado, cidade, bairro, endereco, complemento, email} = req.body // req.body.nome, req.body.email ...
+            if(!nome || !cnpj || !telefone || !cep || !estado || !cidade || !bairro || !endereco || !complemento || !email){
+                res.status(400).json({error: "Informe todos os campos!"})
+                return
+            }
+    
+            const createFornecedor = await Contato.create({nome, cnpj, telefone, cep, estado, cidade, bairro, endereco, complemento, email}) //req.body
+            res.status(201).json(createFornecedor)
+        }
+
+
+        static async updateFornecedor(req,res){
+            const id = parseInt(req.params.id)
+            const contato = await Contato.findByPk(id)
+            if(!fornecedor){
+                res.status(404).json({error:"Não encontrado"})
+                return
+            }
+            
+            const {nome, cnpj, telefone, cep, estado, cidade, bairro, endereco, complemento, email} = req.body // req.body.nome, req.body.email ...
+            if(!nome || !cnpj || !telefone || !cep || !estado || !cidade || !bairro || !endereco || !complemento || !email){
+                res.status(400).json({error: "Informe todos os campos!"})
+                return
+            }
+    
+            const updatedFornecedor = await Fornecedor.update({nome, cnpj, telefone, cep, estado, cidade, bairro, endereco, complemento, email},{where: {id: contato.id}})
+            res.json(updatedFornecedor)
+        }
     }
-
-static getFornecedorById(req, res) {
-    const id = parseInt(req.params.id)
-    const Fornecedor = findByPk(id)
-    if(!Fornecedor) {
-        res.status(404).json({ error: 'Fornecedor não encontrado'})
-        return
-    }
-    res.json(Fornecedor)
-}
-
-static destroyFornecedor(req, res) {
-    const id = parseInt(req.params.id)
-    const Fornecedor = findByPk(id)
-    if(!Fornecedor) {
-        res.status(404).json ({message: 'Fornecedor não encontrado'})
-    }
-    destroy(id)
-    res.json({ message: 'Fornecedor removido com sucesso'})
- }
-
- static updateFornecedor(req, res) {
-    const id = parseInt(req.params.id)
-    const Fornecedor = findByPk(id)
-    if(!Fornecedor){
-        res.status(404).json ({ error: 'Fornecedor não encontrado'})
-    }
- 
-
- const { nome, cnpj, telefone, cep, estado, cidade, bairro, endereco, complemento, email } = req.body
- if(!nome || !cnpj || !telefone || !cep || !estado || !cidade || !bairro || !endereco || !complemento || !email) {
-    res.status(400).json({error: 'Todos os campos são obrigatórios!'})
-    return
- }
-
-    Fornecedor.nome = nome
-    Fornecedor.cnpj = cnpj
-    Fornecedor.telefone = telefone
-    Fornecedor.cep = cep
-    Fornecedor.estado = estado
-    Fornecedor.cidade = cidade
-    Fornecedor.bairro = bairro
-    Fornecedor.endereco = endereco
-    Fornecedor.complemento = complemento
-    Fornecedor.email = email
-
-    update(id,Fornecedor)
-    res.json(Fornecedor)
-}
-}
 
     export default FornecedorController
